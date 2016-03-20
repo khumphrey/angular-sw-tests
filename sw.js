1774dbd0373ba887177f9f375ea6bda17237dbef
khumphrey <khumphrey7890@gmail.com>
2016-03-20 14:04:57 -0400

    working on deleting cache

--- sw.js
+++ sw.js
@@ -12,49 +12,22 @@ self.addEventListener('install', function(event) {
     // We pass a promise to event.waitUntil to signal how 
     // long install takes, and if it failed
     event.waitUntil(
-            //delete current caches and make new ones
-            caches.keys()
-            .then(function(keys) {
-                return Promise.all(
-                    keys.map(function(key) {
-                        return caches.delete(key);
-                    })
-                );
-            })
-            .then(function() {
-                caches.open('simple-sw-v1')
-                    .then(function(cache) {
-                        // And add resources to it
-                        return cache.addAll([
-                            './',
-                            'style.css',
-                            'logging.js',
-                            'script.js',
-                            '/angular/app.js',
-                            '/angular/state.js',
-                            '/doc-imgs/Hippo.jpg',
-                            '/node_modules/angular/angular.min.js',
-                            '/node_modules/angular-ui-router/release/angular-ui-router.min.js',
-                            '/angular/pony.template.html',
-                            '/doc-imgs/pony.jpg'
-                        ]);
-                    })
-            })
-        )
-        // event.waitUntil(
-        //     // We open a cache…
-        // );
-});
-
-self.addEventListener('install', function(event) {
-    // delete all the caches
-    event.waitUntil(
-        caches.keys().then(function(keys) {
-            return Promise.all(
-                keys.map(function(key) {
-                    return caches.delete(key);
-                })
-            );
+        // We open a cache…
+        caches.open('simple-sw-v1').then(function(cache) {
+            // And add resources to it
+            return cache.addAll([
+                './',
+                'style.css',
+                'logging.js',
+                'script.js',
+                '/angular/app.js',
+                '/angular/state.js',
+                '/doc-imgs/Hippo.jpg',
+                '/node_modules/angular/angular.min.js',
+                '/node_modules/angular-ui-router/release/angular-ui-router.min.js',
+                '/angular/pony.template.html',
+                '/doc-imgs/pony.jpg'
+            ]);
         })
     );
 });
@@ -68,29 +41,30 @@ self.addEventListener('activate', function(event) {
 // ServiceWorker's scope, and any request made within that
 // page
 self.addEventListener('fetch', function(event) {
-            // if (/\.jpg$/.test(event.request.url)) {
-            //     event.respondWith(fetch('trollface.svg'));
-            //     return;
-            // }
-            // var pageURL = new URL('./', location);
+    // if (/\.jpg$/.test(event.request.url)) {
+    //     event.respondWith(fetch('trollface.svg'));
+    //     return;
+    // }
+    // var pageURL = new URL('./', location);
 
-            // if (event.request.url === pageURL.href) {
-            //     event.respondWith(new Response("Hello world!"))
-            //     return;
-            // }
-            // Calling event.respondWith means we're in charge
-            // of providing the response. We pass in a promise
-            // that resolves with a response object  
-            event.respondWith(
-                // First we look for something in the caches that
-                // matches the request
-                caches.match(event.request)
-                .then(function(response) {
-                    console.log('event.request: ', event.request);
-                    // If we get something, we return it, otherwise
-                    // it's null, and we'll pass the request to
-                    // fetch, which will use the network.
-                    console.log('is there a response', response);
-                    return response || fetch(event.request);
-                })
-            )
+    // if (event.request.url === pageURL.href) {
+    //     event.respondWith(new Response("Hello world!"))
+    //     return;
+    // }
+    // Calling event.respondWith means we're in charge
+    // of providing the response. We pass in a promise
+    // that resolves with a response object  
+    event.respondWith(
+        // First we look for something in the caches that
+        // matches the request
+        caches.match(event.request)
+        .then(function(response) {
+            console.log('event.request: ', event.request);
+            // If we get something, we return it, otherwise
+            // it's null, and we'll pass the request to
+            // fetch, which will use the network.
+            console.log('is there a response', response);
+            return response || fetch(event.request);
+        })
+    );
+});
